ec143600237f2d3f4b7300ab77f28c0478d94dab
Jonathan Olson <jonathan.olson@colorado.edu>
2024-12-10 20:38:20 -0700

    improved initialization

--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,36 @@
-import './init.js';
 import 'scenerystack/splash';
-import { asyncLoader } from "scenerystack/phet-core";
-import { Screen, ScreenOptions, ScreenView, ScreenViewOptions, Sim, SimOptions } from "scenerystack/sim";
+import { onReadyToLaunch, Screen, ScreenOptions, ScreenView, ScreenViewOptions, Sim, SimOptions } from 'scenerystack/sim';
 import { ResetAllButton } from "scenerystack/scenery-phet";
 import { StringProperty } from 'scenerystack/axon';
 import { Tandem } from 'scenerystack/tandem';
+import { brand, TBrand } from 'scenerystack/brand';
+import { logo_png, logoOnWhite_png } from 'scenerystack/adapted-from-phet';
 
-// Signify that the simLauncher was called, see https://github.com/phetsims/joist/issues/142
-window.phet.joist.launchCalled = true;
+const Brand: TBrand = {
 
-console.log( 'tests' );
+  // Nickname for the brand, which should match the brand subdirectory name, grunt option for --brand as well as the
+  // query parameter for ?brand.  This is used in Joist to provide brand-specific logic, such as what to show in the
+  // About dialog, decorative text around the PhET button, and whether to check for updates.
+  id: 'adapted-from-phet',
+
+  // Optional string for the name of the brand.  If non-null, the brand name will appear in the top of the About dialog
+  // {string} For example: "My Company"
+  name: null,
+
+  // Optional string for the copyright statement.  If non-null, it will appear in the About dialog
+  // {string} For example: "Copyright © 2014, My Company"
+  copyright: null,
+
+  /**
+   * Return any links to appear in the About dialog.  The sim name and locale can be used for customization if desired.
+   * For example: { textStringProperty: new Property( "My Company Support" ), url: "https://www.mycompany.com/support" }
+   */
+  getLinks: function() { return []; },
+  logoOnBlackBackground: logo_png,
+  logoOnWhiteBackground: logoOnWhite_png
+};
+
+brand.register( 'Brand', Brand );
 
 class DemoSimModel {
   public reset(): void {
@@ -56,11 +77,7 @@ class DemoSimScreen extends Screen<DemoSimModel, DemoSimScreenView> {
   }
 }
 
-const unlockLaunch = asyncLoader.createLock( { name: 'launch' } );
-
-// Add listener before unlocking the launch lock
-asyncLoader.addListener( () => {
-
+onReadyToLaunch( () => {
   const titleStringProperty = new StringProperty( 'Demo Simulation' );
 
   const screens = [
@@ -84,7 +101,4 @@ asyncLoader.addListener( () => {
 
   const sim = new Sim( titleStringProperty, screens, options );
   sim.start();
-
-
 } );
-unlockLaunch();
