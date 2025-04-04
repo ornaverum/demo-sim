6fa0c23d8bca9f3b441827e59e5f766f015f4505
Jonathan Olson <jonathan.olson@colorado.edu>
2025-04-04 15:04:57 -0600

    demo-sim bump to scenerystack 2.0.0-alpha.1

--- src/init.ts
+++ src/init.ts
@@ -3,6 +3,7 @@ import type { TBrand } from "scenerystack/brand";
 ( async () => {
   const init = ( await import( "scenerystack/init" ) ).init;
 
+  // Initialize values that will be used at import-time by other modules.
   init({
     // Internal name of the simulation.
     name: "demo-sim",
@@ -26,39 +27,52 @@ import type { TBrand } from "scenerystack/brand";
     allowLocaleSwitching: true,
   });
 
+  // Enable assertions (done early so other failures are detected).
+  ( await import( "scenerystack/assert" ) ).enableAssert();
+
+  // Creates a lock that will be held until the sim is ready to launch. This allows waiting to launch the sim until all
+  // resources have loaded (and e.g. images have dimensions, sounds are ready to play, etc.)
+  await import( "scenerystack/lock" );
+
+  // Load the splash screen, which will be shown while the sim is loading.
   await import( "scenerystack/splash" );
-  const brand = ( await import( "scenerystack/brand" ) ).brand;
-  const adaptedFromPhetModule = await import( "scenerystack/adapted-from-phet" );
 
-  const logo_png = adaptedFromPhetModule.logo_png;
-  const logoOnWhite_png = adaptedFromPhetModule.logoOnWhite_png;
+  // Initialize branding information
+  {
+    const brand = ( await import( "scenerystack/brand" ) ).brand;
+    const adaptedFromPhetModule = await import( "scenerystack/adapted-from-phet" );
 
-  const Brand: TBrand = {
-    // Nickname for the brand, which should match the brand subdirectory name, grunt option for --brand as well as the
-    // query parameter for ?brand.  This is used in Joist to provide brand-specific logic, such as what to show in the
-    // About dialog, decorative text around the PhET button, and whether to check for updates.
-    id: "adapted-from-phet",
+    const logo_png = adaptedFromPhetModule.logo_png;
+    const logoOnWhite_png = adaptedFromPhetModule.logoOnWhite_png;
 
-    // Optional string for the name of the brand.  If non-null, the brand name will appear in the top of the About dialog
-    // {string} For example: "My Company"
-    name: null,
+    const Brand: TBrand = {
+      // Nickname for the brand, which should match the brand subdirectory name, grunt option for --brand as well as the
+      // query parameter for ?brand.  This is used in Joist to provide brand-specific logic, such as what to show in the
+      // About dialog, decorative text around the PhET button, and whether to check for updates.
+      id: "adapted-from-phet",
 
-    // Optional string for the copyright statement.  If non-null, it will appear in the About dialog
-    // {string} For example: "Copyright © 2014, My Company"
-    copyright: null,
+      // Optional string for the name of the brand.  If non-null, the brand name will appear in the top of the About dialog
+      // {string} For example: "My Company"
+      name: null,
 
-    /**
-     * Return any links to appear in the About dialog.  The sim name and locale can be used for customization if desired.
-     * For example: { textStringProperty: new Property( "My Company Support" ), url: "https://www.mycompany.com/support" }
-     */
-    getLinks: function () {
-      return [];
-    },
-    logoOnBlackBackground: logo_png,
-    logoOnWhiteBackground: logoOnWhite_png,
-  };
+      // Optional string for the copyright statement.  If non-null, it will appear in the About dialog
+      // {string} For example: "Copyright © 2014, My Company"
+      copyright: null,
 
-  brand.register("Brand", Brand);
+      /**
+       * Return any links to appear in the About dialog.  The sim name and locale can be used for customization if desired.
+       * For example: { textStringProperty: new Property( "My Company Support" ), url: "https://www.mycompany.com/support" }
+       */
+      getLinks: function () {
+        return [];
+      },
+      logoOnBlackBackground: logo_png,
+      logoOnWhiteBackground: logoOnWhite_png,
+    };
 
+    brand.register("Brand", Brand);
+  }
+
+  // Load the main content.
   await import( './main.ts' );
 } )();
